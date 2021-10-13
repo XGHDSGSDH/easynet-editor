cd38a802432dd71918c4d72574dfed9cf310566b
XGHDSGSDH <74358408+XGHDSGSDH@users.noreply.github.com>
2021-10-13 22:12:42 +0800

    Add files via upload

--- editor.py
+++ editor.py
@@ -1,4 +1,4 @@
-﻿from tkinter import *
+from tkinter import *
 import tkinter.ttk
 from tensor import tensor
 from layer import layer
@@ -25,7 +25,7 @@ class editor:
         #layer按钮
         self.btn_layer=Button(self.window,activeforeground="black",text="add_layer",font=("Arial Bold",16),bg="blue",fg="black")
         self.btn_layer.grid(row=0,column=2)
-        self.btn_layer.bind("<Button-1>",self.add_layer)
+        self.btn_layer.bind("<Button-1>",self.question)
         
         #相对坐标系坐标
         self.center_x=x_pos
@@ -43,53 +43,109 @@ class editor:
         #拖动时暂时坐标
         self.register_x=0
         self.register_y=0
+        
+        #输入窗口
+        self.question_window=0
+        self.caution_end=0
+        self.caution_front=0
+        self.question_end=0
+        self.question_window=0
+        self.button_for_quit=0
     
+    def question(self,event):
+        if self.state==-1 or self.layer_combobox.current()==3 or self.layer_combobox.current()==-1:
+            return
+        self.state=-1
+        print(self.state)
+        self.question_window=Tk()
+        self.question_window.title("layer information")
+        self.caution_front=Label(self.question_window,text="起点")
+        self.caution_front.grid(row=0,sticky=W)
+        self.question_front=Entry(self.question_window)
+        self.question_front.grid(row=0,column=1,sticky=E)
+        self.question_front.insert(0,"1")
+        
+        self.caution_end=Label(self.question_window,text="终点")
+        self.caution_end.grid(row=1,sticky=W)
+        self.question_end=Entry(self.question_window)
+        self.question_end.grid(row=1,column=1,sticky=E)
+        self.question_end.insert(0,"1")
+        
+        self.button_for_quit=Button(self.question_window,text="确定")
+        self.button_for_quit.grid(row=2,column=0)
+        self.button_for_quit.bind("<ButtonRelease-1>",self.add_layer)
+        self.question_window.mainloop()
+        
     def add_tensor(self,event):
+        if self.state==-1:
+            return
         self.window.update()
-        self.vector_tensor.append(tensor(self.window.winfo_x()+self.window.winfo_width()/2, self.window.winfo_y()+self.window.winfo_height()/2, self.tensor_num+1, self))
         self.tensor_num+=1
+        self.vector_tensor.append(tensor(self.window.winfo_x()+self.window.winfo_width()/2, self.window.winfo_y()+self.window.winfo_height()/2, self.tensor_num, self))
         print("Successfully added a tensor")
     
     def add_layer(self,event):
-        if self.layer_combobox.current()==3:
-            return
+        if int(self.question_front.get())==int(self.question_end.get()):
+            return 0;
+        print(self.layer_combobox.current())
         self.window.update()
         self.layer_num+=1
-        self.vector_layer.append(layer(self.window.winfo_x()+self.window.winfo_width()/2,self.window.winfo_y()+self.window.winfo_height()/2,self.layer_combobox.get(),1,1,self.layer_num, self))
+        self.vector_layer.append(layer(self.window.winfo_x()+self.window.winfo_width()/2,self.window.winfo_y()+self.window.winfo_height()/2,self.layer_combobox.get(),int(self.question_front.get()),int(self.question_end.get()),self.layer_num,self))
         print("Successfully added a layer")
+        self.vector_tensor[int(self.question_end.get())-1].inlayer.append(self.vector_layer[self.layer_num-1])
+        self.vector_tensor[int(self.question_front.get())-1].outlayer.append(self.vector_layer[self.layer_num-1])
+        self.question_window.destroy()
+        self.caution_end=0
+        self.caution_front=0
+        self.question_end=0
+        self.question_window=0
+        self.button_for_quit=0
+        self.state=0
     
     def redraw(self):
         self.rander_target.delete(ALL)
         for i in self.vector_tensor:
             #self.center_x+=100
-            i.draw_my_self(self.center_x,self.center_y)
+            i.draw_my_self()
         for i in self.vector_layer:
-            i.draw_my_self(self.center_x,self.center_y)
+            i.draw_my_self()
     
     def press_down(self,event):
-        print("get a press down")
+        #print("get a press down")
+        print(self.state)
+        if self.state==-1:
+            return 
         self.state=0
         self.now_pressed=0
         for i in self.vector_tensor:
             #print("fuck")
-            if i.in_my_area(self.center_x,self.center_y,event.x,event.y)==1:
+            if i.in_my_area(event.x,event.y)==1:
                 self.state=1
                 self.now_pressed=i.num
         for i in self.vector_layer:
-            if i.in_my_area(self.center_x,self.center_y,event.x,event.y)==1:
-                self.state==2
+            if i.in_my_area(event.x,event.y)==1:
+                self.state=2
                 self.now_pressed=i.num
         self.register_x=event.x
         self.register_y=event.y
         print(self.state,self.now_pressed)#现在的状态和已选中的点的编号
     def press_motion(self,event):
-        print("get a left click motion")
+        #print("get a left click motion")
+        if self.state==-1:
+            return 
         if self.state==0:
             self.center_x+=event.x-self.register_x
             self.center_y+=event.y-self.register_y
             self.register_x=event.x
             self.register_y=event.y
             self.redraw()
+            return
+        if self.state==1:
+            self.vector_tensor[self.now_pressed-1].delete()
+            self.vector_tensor[self.now_pressed-1].follow(event.x,event.y)
+            return
+        if self.state==2:
+            self.vector_layer[self.now_pressed-1].delete()
+            self.vector_layer[self.now_pressed-1].follow(event.x,event.y)
     def press_up(self,event):
         print(0)
-    
--- layer.py
+++ layer.py
@@ -1,4 +1,5 @@
-﻿class layer:
+from tkinter import *
+class layer:
     def __init__(self,x,y,name,front,end,num,neweditor):
         self.new_editor = neweditor
         self.radius=30
@@ -10,12 +11,58 @@
         self.num=num#编号
         self.circle=self.new_editor.rander_target.create_oval(x-self.radius,y-self.radius,x+self.radius,y+self.radius,fill="orange")
         self.text=self.new_editor.rander_target.create_text(x,y,text=name)
+        self.arrow1=draw_line1(self)
+        self.arrow2=draw_line2(self)
         #这里调用了别的类中的函数之后可以修改一下
-    def draw_my_self(self,x,y):#重新画自己的圆和字
-        self.circle=self.new_editor.rander_target.create_oval(self.cmpx+x-self.radius,self.cmpy+y-self.radius,x+self.cmpx+self.radius,self.cmpy+y+self.radius,fill="orange")
-        self.text=self.new_editor.rander_target.create_text(self.cmpx+x,self.cmpy+y,text=self.type)
-
-    def in_my_area(self,x,y,click_x,click_y):#检测点击是否在自己范围内
-        if (self.cmpx+x-click_x)**2+(self.cmpy+y-click_y)**2<=self.radius**2:
+    def draw_my_self(self):#重新画自己的圆和字和箭头
+        self.circle=self.new_editor.rander_target.create_oval(self.cmpx+self.new_editor.center_x-self.radius,self.cmpy+self.new_editor.center_y-self.radius,self.new_editor.center_x+self.cmpx+self.radius,self.cmpy+self.new_editor.center_y+self.radius,fill="orange")
+        self.text=self.new_editor.rander_target.create_text(self.cmpx+self.new_editor.center_x,self.cmpy+self.new_editor.center_y,text=self.type)
+        self.arrow1=draw_line1(self)
+        self.arrow2=draw_line2(self)
+        
+    def in_my_area(self,click_x,click_y):#检测点击是否在自己范围内
+        if (self.cmpx+self.new_editor.center_x-click_x)**2+(self.cmpy+self.new_editor.center_y-click_y)**2<=self.radius**2:
             return 1
         return 0
+    def delete(self):
+        self.new_editor.rander_target.delete(self.circle)
+        self.new_editor.rander_target.delete(self.text)
+        self.new_editor.rander_target.delete(self.arrow1)
+        self.new_editor.rander_target.delete(self.arrow2)
+    def follow(self,x,y):
+        self.cmpx=x-self.new_editor.center_x
+        self.cmpy=y-self.new_editor.center_y
+        self.circle=self.new_editor.rander_target.create_oval(x-self.radius,y-self.radius,x+self.radius,y+self.radius,fill="orange")
+        self.text=self.new_editor.rander_target.create_text(x,y,text=self.type)
+        self.arrow1=draw_line1(self)
+        self.arrow2=draw_line2(self)
+        
+def draw_line1(layera):
+    #print("fuck")
+    x1=layera.cmpx+layera.new_editor.center_x
+    y1=layera.cmpy+layera.new_editor.center_y
+    xs=layera.new_editor.vector_tensor[layera.front-1].cmpx+layera.new_editor.center_x
+    ys=layera.new_editor.vector_tensor[layera.front-1].cmpy+layera.new_editor.center_y
+    if (xs-x1)**2+(ys-y1)**2<=layera.radius+layera.new_editor.vector_tensor[layera.front-1].radius:
+        return 0
+    k=((ys-y1)**2+(xs-x1)**2)**0.5
+    return layera.new_editor.rander_target.create_line(
+        (xs+(x1-xs)/k*layera.new_editor.vector_tensor[layera.front-1].radius,
+         ys+(y1-ys)/k*layera.new_editor.vector_tensor[layera.front-1].radius,
+         x1-(x1-xs)/k*layera.radius,
+         y1-(y1-ys)/k*layera.radius),
+         arrow=LAST)
+def draw_line2(layera):
+    x1=layera.cmpx+layera.new_editor.center_x
+    y1=layera.cmpy+layera.new_editor.center_y
+    xe=layera.new_editor.vector_tensor[layera.end-1].cmpx+layera.new_editor.center_x
+    ye=layera.new_editor.vector_tensor[layera.end-1].cmpy+layera.new_editor.center_y
+    if (xe-x1)**2+(ye-y1)**2<=layera.radius+layera.new_editor.vector_tensor[layera.end-1].radius:
+        return 0
+    k=((ye-y1)**2+(xe-x1)**2)**0.5
+    return layera.new_editor.rander_target.create_line(
+        (xe+(x1-xe)/k*layera.new_editor.vector_tensor[layera.end-1].radius,
+         ye+(y1-ye)/k*layera.new_editor.vector_tensor[layera.end-1].radius,
+         x1-(x1-xe)/k*layera.radius,
+         y1-(y1-ye)/k*layera.radius),
+         arrow=FIRST)
